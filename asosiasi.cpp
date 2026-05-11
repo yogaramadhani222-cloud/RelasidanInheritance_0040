16d6b56626ee7d9ca72d59fb0ffa5389aaa2a08e
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-11 12:04:06 +0700

    implementasi void dokter tambah pasien dan  cetak pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -47,3 +47,17 @@ void pasien::cetakDokter()
     }
     cout << endl;
 }
+void dokter::tambahPasien(pasien* pPasien)
+{
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+void dokter::cetakPasien()
+{
+    cout << "Daftar pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien)
+    {
+        cout << "- " << a->nama << "\n";
+    }
+    cout << endl;
+}
