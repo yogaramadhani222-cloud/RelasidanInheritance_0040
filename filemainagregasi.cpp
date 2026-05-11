4d753f15bdb001166a773596f6f99c9b0fb07890
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-11 11:54:29 +0700

    menambah varibu tambah anak dan cetak anak

--- filemainagregasi.cpp
+++ filemainagregasi.cpp
@@ -12,7 +12,14 @@ int main() //agregasi
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("Dewi");
 
-    
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varibu2->tambahAnak(varAnak3);
+    varibu2->tambahAnak(varAnak2);
+
+    varIbu->cetakAnak();
+    varibu2->cetakAnak();
+
     system("pause");
     return 0;
 }
