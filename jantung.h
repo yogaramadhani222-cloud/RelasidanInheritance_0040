1868bf2afa5e5bd2be129164efe4803f286838a8
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-11 12:12:44 +0700

    membuat kelas jantung dan deklarasi jantung

---
+++ jantung.h
@@ -0,0 +1,17 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung {
+public:
+    jantung()
+    {
+        cout << "Jantung dihidupkan\n";
+    }
+    ~jantung()
+    {
+        cout << "Jantung dimatikan\n";
+    }
+};
+#endif
