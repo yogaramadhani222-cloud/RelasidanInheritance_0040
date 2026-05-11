0ace57dc911452df23a1fc6e1ae6723886c55930
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-11 12:11:54 +0700

    mengisi fungsi main

--- inheritance.cpp
+++ inheritance.cpp
@@ -41,5 +41,9 @@ public:
 };
 
 int main(){
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
     return 0;
 }
