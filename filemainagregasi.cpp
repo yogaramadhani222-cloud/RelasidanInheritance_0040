01fcdb722be1eff7b8686322c8ef15d5ef951deb
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-11 11:52:11 +0700

    menambahkan var anak 1, 2 dan 3

--- filemainagregasi.cpp
+++ filemainagregasi.cpp
@@ -8,7 +8,11 @@ int main() //agregasi
 {
     ibu* varIbu = new ibu("Dini");
     ibu* varibu2 = new ibu("Novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("Dewi");
 
+    
     system("pause");
     return 0;
 }
