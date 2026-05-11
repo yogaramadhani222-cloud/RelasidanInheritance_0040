30495d55cc3aa11da314d86a43c973aed79fdec4
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-11 11:48:13 +0700

    menambah tampilan cetak anak() dan perulangan

--- ibu.h
+++ ibu.h
@@ -22,4 +22,17 @@ void ibu::tambahAnak(anak* pAnak)
 {
     daftar_anak.push_back(pAnak);
 }
+void ibu::cetakAnak()
+{
+    cout << "Daftar anak dari ibu \"" << this->nama << "\":\n";
+    /*for (auto& a : daftar_anak)
+    {
+        cout << "- " << a->nama << "\n";
+    }*/
+   for (int i = 0; i < daftar_anak.size(); i++)
+   {
+       cout << daftar_anak[i]->nama << endl;
+   }
+   cout << endl;
+}
 #endif
