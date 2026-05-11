28dd29d0e6b9cff802a72004bd1a286afbbed2e0
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-11 12:04:53 +0700

    mengisi int main dan final program

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -61,3 +61,27 @@ void dokter::cetakPasien()
     }
     cout << endl;
 }
+
+int main()
+{
+    dokter* varDokter1 = new dokter("Dr.Budi");
+    dokter* varDokter2 = new dokter("Dr.tono");
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
+
+    varDokter1->tambahPasien(varPasien1);
+    varDokter1->tambahPasien(varPasien2);
+    varDokter2->tambahPasien(varPasien1);
+
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
+}
