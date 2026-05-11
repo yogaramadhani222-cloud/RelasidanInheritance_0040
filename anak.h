dc4421bbb4876f6a74de15911be349399651fac1
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-11 11:38:38 +0700

    membuat class anak

---
+++ anak.h
@@ -0,0 +1,12 @@
+#ifndef ANAK_H
+#define ANAK_H
+
+class anak {
+public:
+    string nama;
+    anak(string pNama) :nama(pNama)
+    {
+        cout << "Anak\"" << nama << "\"ada\n";
+    }
+};
+#endif
